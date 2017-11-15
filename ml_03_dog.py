eeb518f45afeda8354de5debb65e802112e8470d
linbo0518 <linbo0518@gmail.com>
2017-11-15 14:41:09 +0800

    format code with yapf

--- ml_03_dog.py
+++ ml_03_dog.py
@@ -7,5 +7,6 @@ lab_num = 500
 greyhound_height_data = 28 + 4 * np.random.randn(greyhound_num)
 lab_geight_data = 24 + 4 * np.random.randn(lab_num)
 
-plt.hist([greyhound_height_data, lab_geight_data], stacked=True, color=(['r', 'b']))
+plt.hist(
+    [greyhound_height_data, lab_geight_data], stacked=True, color=(['r', 'b']))
 plt.show()
