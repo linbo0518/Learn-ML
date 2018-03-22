94bbc293f5b23e53cb1e97e9c885957b8a3fd461
linbo0518 <linbo0518@gmail.com>
2018-03-22 17:40:57 +0800

    del foundation

--- toy examples/foundation/01.apple_or_orange.py
+++
@@ -1,8 +0,0 @@
-from sklearn import tree
-
-features = [[140, 0], [130, 0], [150, 1], [170, 1]]
-labels = ['apple', 'apple', 'orange', 'orange']
-classifier = tree.DecisionTreeClassifier()
-classifier = classifier.fit(features, labels)
-
-print(classifier.predict([[160, 1]]))
